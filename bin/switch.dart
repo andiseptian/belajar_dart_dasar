363a61878b4c56dfa9594dc91c088cd302d59e34
andiseptian <andiseptianadichandra@gmail.com>
2024-02-03 14:32:51 +0800

    menambahkan file switch.dart

---
+++ bin/switch.dart
@@ -0,0 +1,16 @@
+void main() {
+  var nilai = 'A';
+  switch (nilai) {
+    case 'A':
+      print('lulus dengan sangat baik');
+      break;
+    case 'B':
+      print('lulus dengan baik');
+      break;
+    case 'C':
+      print('lulus dengan cukup');
+      break;
+    default:
+      print('mungkin anda salah jurusan');
+  }
+}
