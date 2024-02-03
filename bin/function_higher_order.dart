3fc36111808c688db526e5f2895929593d2aba77
andiseptian <andiseptianadichandra@gmail.com>
2024-02-03 15:39:09 +0800

    menambahkan file function higher order

---
+++ bin/function_higher_order.dart
@@ -0,0 +1,17 @@
+void sayHello(String name, String Function(String) filter) {
+  var filteredName = filter(name);
+  print('hi $filteredName');
+}
+
+String filterBadWord(String name) {
+  if (name == 'gila') {
+    return '***';
+  } else {
+    return name;
+  }
+}
+
+void main() {
+  sayHello('Andi', filterBadWord);
+  sayHello('gila', filterBadWord);
+}
